b2ab14a085393efd211b0498de58cc8bf6eca2fc
Soroush Jaberi <sorooshjaberi@gmail.com>
2024-06-14 19:09:39 +0330

    load balancer

--- main.go
+++ main.go
@@ -2,16 +2,66 @@ package main
 
 import (
 	"fmt"
-	rolemanager "github.com/sorooshjaberi/GoRoleManager/RoleManager"
+	"sync"
+	"time"
 )
 
+var names = []string{"Zan", "Zendegi", "Azadi", "Mahsa", "Hameye dokhtarane iran", "Zan", "Zendegi", "Azadi", "Mahsa", "Hameye dokhtarane iran", "Zan", "Zendegi", "Azadi", "Mahsa", "Hameye dokhtarane iran", "Zan", "Zendegi", "Azadi", "Mahsa", "Hameye dokhtarane iran", "Zan", "Zendegi", "Azadi", "Mahsa", "Hameye dokhtarane iran", "Zan", "Zendegi", "Azadi", "Mahsa", "Hameye dokhtarane iran"}
+
 func main() {
+	dataChan := make(chan string)
+	signalCreateReader := make(chan bool)
 
+	wg := new(sync.WaitGroup)
 
-	roles := new(rolemanager.Role).Init()
-	perms := roles.GetPermisions(5)
+	writer := func(wg *sync.WaitGroup, dataChan chan<- string) {
 
-	fmt.Printf("perms: %v\n", perms)
+		defer wg.Done()
+		defer close(dataChan)
+		defer close(signalCreateReader)
+
+		// wait for 1 second and if delay,
+		// aka. tolerance
+		timeout := time.Millisecond * 500
+
+		for _, name := range names {
+			select {
+			case dataChan <- name:
+			case <-time.After(timeout):
+				// if timed out
+				signalCreateReader <- true
+			}
+		}
+	}
+	reader := func(wg *sync.WaitGroup, dataChan <-chan string, timeout time.Duration) {
+		defer wg.Done()
+		defer fmt.Println("Bye!!!!!!")
+		 fmt.Println("Hey !!!!!!")
+		for {
+			// reader delay
+			time.Sleep(time.Second)
+
+			select {
+			case name, ok := <-dataChan:
+				if !ok {
+					return
+				}
+				fmt.Printf("name: %v\n", name)
+			// timeout for idle reader
+			case <-time.After(timeout):
+				return
+			}
+		}
+
+	}
+	wg.Add(1)
+	go writer(wg, dataChan)
+
+	for range signalCreateReader {
+		wg.Add(1)
+		go reader(wg, dataChan, time.Millisecond*1000)
+	}
+
+	wg.Wait()
 
 }
-
